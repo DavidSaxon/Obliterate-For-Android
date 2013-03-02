14dff4173c154d05989d8730560c19374fcbef3f
David Saxon <davidesaxon@gmail.com>
2013-03-02 23:13:50 +1300

    completed dividers

--- src/nz/co/withfire/obliterate/entities/menu/Divider.java
+++ src/nz/co/withfire/obliterate/entities/menu/Divider.java
@@ -1,5 +1,6 @@
 package nz.co.withfire.obliterate.entities.menu;
 
+import android.opengl.Matrix;
 import nz.co.withfire.obliterate.entities.Entity;
 import nz.co.withfire.obliterate.graphics.drawable.shape2d.Quad2d;
 import nz.co.withfire.obliterate.utilities.Vector2d;
@@ -17,6 +18,12 @@ public class Divider extends Entity {
     //the image of the divider
     private Quad2d image;
     
+    //Matrix
+    //the model view projection matrix
+    private float[] mvpMatrix = new float[16];
+    //the translation matrix
+    private float[] tMatrix = new float[16];
+    
     //CONSTUCTOR
     public Divider(Vector2d pos, Vector2d dim, Vector2d bgPos) {
         
@@ -25,8 +32,30 @@ public class Divider extends Entity {
         this.bgPos = bgPos;
         
         //create the divider quad
+        float quadCoord[] = {   -dim.getX(),  dim.getY(), 0.0f,
+                -dim.getX(), -dim.getY(), 0.0f,
+                 dim.getX(), -dim.getY(), 0.0f,
+                 dim.getX(),  dim.getY(), 0.0f};
+        float colour[] = {  0.4f, 0.4f, 0.4f, 1.0f,
+                            0.4f, 0.4f, 0.4f, 1.0f,
+                            0.4f, 0.4f, 0.4f, 1.0f,
+                            0.4f, 0.4f, 0.4f, 1.0f };
+        
+        image = new Quad2d(quadCoord, colour);
     }
     
     //METHODS
-    
+    @Override
+    public void draw(float[] viewMatrix, float[] projectionMatrix) {
+        
+       //shift into visible range
+        Matrix.setIdentityM(tMatrix, 0);
+        Matrix.translateM(tMatrix, 0, pos.getX(), pos.getY(), -0.01f);
+        
+        //multiply the matrix
+        Matrix.multiplyMM(mvpMatrix, 0, tMatrix, 0, viewMatrix, 0);
+        Matrix.multiplyMM(mvpMatrix, 0, projectionMatrix, 0, mvpMatrix, 0);
+        
+        image.draw(mvpMatrix);
+    }
 }
